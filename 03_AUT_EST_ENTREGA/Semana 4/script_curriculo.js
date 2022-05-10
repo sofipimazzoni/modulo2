483cac3c81a5a177b14035e7f7c6c13c2fc1e5c3
sofipimazzoni <83848719+sofipimazzoni@users.noreply.github.com>
2022-05-10 13:17:11 -0300

    atividade dessa semana

---
+++ 03_AUT_EST_ENTREGA/Semana 4/script_curriculo.js
@@ -0,0 +1,15 @@
+console.log("entrou no js");
+
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        addlinha();
+    }
+}
+
+function addlinha(){
+    var conteudo_html = "<h2> Desenvolvendo no momento </h2>"
+    conteudo_html += "<p> No momento estou desenvolvendo um projeto para uma ONG chamada Revirar, que cuida de pessoas em situação de rua. O objetivo é fazer um site que possua uma área administrativa para organizar a ficha de cada assistido</p>"
+    document.getElementById("novalinha").innerHTML = conteudo_html;
+    
+}
